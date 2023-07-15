53d1e42f0492d85d80a01cbcac62ffbc5971c32a
tomerr90 <37086811+tomerr90@users.noreply.github.com>
2023-07-16 00:06:00 +0300

    Update run process

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,6 @@
-java --enable-preview --add-modules=jdk.incubator.vector -cp test.jar main.SimpleSum > SimpleSum
-java --enable-preview --add-modules=jdk.incubator.vector -cp test.jar main.SimpleSum > SimpleSumNoSuperWord
-java --enable-preview --add-modules=jdk.incubator.vector -cp test.jar main.ComplexExpression > ComplexExpression
-java --enable-preview --add-modules=jdk.incubator.vector -cp test.jar main.ComplexExpression > ComplexExpressionNoSuperWord
-java --enable-preview --add-modules=jdk.incubator.vector -cp test.jar main.ArrayStats > ArrayStats
+mvn clean package
+java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.SimpleSum > SimpleSum
+java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.SimpleSum > SimpleSumNoSuperWord
+java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.ComplexExpression > ComplexExpression
+java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.ComplexExpression > ComplexExpressionNoSuperWord
+java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.ArrayStats > ArrayStats
