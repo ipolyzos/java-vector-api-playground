db6555f4a6072487d49d9e687c14a7485864f3c2
tomerr90 <37086811+tomerr90@users.noreply.github.com>
2023-07-16 00:06:45 +0300

    Adding missing target prefix

--- runTests.sh
+++ runTests.sh
@@ -1,6 +1,6 @@
 mvn clean package
-java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.SimpleSum > SimpleSum
-java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.SimpleSum > SimpleSumNoSuperWord
-java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.ComplexExpression > ComplexExpression
-java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.ComplexExpression > ComplexExpressionNoSuperWord
-java --enable-preview --add-modules=jdk.incubator.vector -cp vectors.jar main.ArrayStats > ArrayStats
+java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.SimpleSum > SimpleSum
+java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.SimpleSum > SimpleSumNoSuperWord
+java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.ComplexExpression > ComplexExpression
+java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.ComplexExpression > ComplexExpressionNoSuperWord
+java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.ArrayStats > ArrayStats
