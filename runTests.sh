c7f7a245c7729d55e0eef2c0f2a242968beecd24
Ioannis Polyzos <git@ipolyzos.com>
2024-09-14 21:39:52 +0300

    Remove runTests.sh to keep the base clean
    - Deleted the redundant runTests.sh script.
    - Ensured the project base remains clean and maintainable.
    
    Signed-off-by: Ioannis Polyzos <git@ipolyzos.com>

--- runTests.sh
+++
@@ -1,6 +0,0 @@
-mvn clean package
-java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.SimpleSum > SimpleSum
-java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.SimpleSum > SimpleSumNoSuperWord
-java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.ComplexExpression > ComplexExpression
-java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.ComplexExpression > ComplexExpressionNoSuperWord
-java --enable-preview --add-modules=jdk.incubator.vector -cp target/vectors.jar main.ArrayStats > ArrayStats
